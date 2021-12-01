8eb02b22a6d93d24c6b0770569049cf7a5a983e5
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2021-12-01 01:48:00 +0100

    Minor style fixes

--- srv6_sdn_control_plane/interface_discovery/interface_discovery.py
+++ srv6_sdn_control_plane/interface_discovery/interface_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (C) 2018 Carmine Scarpitta, Pier Luigi Ventre, Stefano Salsano - (CNIT and University of Rome 'Tor Vergata')
+# Copyright (C) 2018 Carmine Scarpitta, Pier Luigi Ventre, Stefano Salsano - (CNIT and University of Rome "Tor Vergata")
 #
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
@@ -10,7 +10,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
